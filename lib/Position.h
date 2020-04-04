59c89db6b9aaccaa60e3d66db0c6aef0835868f0
MiharyR <mihary.rnvsn@gmail.com>
2020-04-04 12:03:37 +0200

    prototypes made

--- lib/Position.h
+++ lib/Position.h
@@ -10,36 +10,39 @@ LAST MODIFICATION:            ||
 #define __POSITION__
 
 #include "Company.h"
-
 #include <iostream>
 #include <vector>
 
 
 class Position
 {
-
 	public:
 		// Creation
-			Position( std::string name, std::vector<std::string> _skills, Company _company ) ;
+			Position( std::string const& title, Company const& company ) ;
+			Position( std::string const& title, Company const& company, std::vector<std::string> const& skills ) ;
+
+		// Deletion
+			void delete_position() ;
+			~Position() ;																				// (  IL FAUDRA NOTAMMENT DETRUIRE '_listSkills' PROPREMENT DE SORTE QU'IL N'Y AIT PAS DE FUITE DE MEMOIRE  )
 
 		// Name
-			std::string get_name() ;
-			void set_name( std::string company ) ;
+			std::string get_title() const ;
+			void set_title( std::string const& title ) ;
 		
-		// Skills
-			void print_skills() ;
-			void add_skills( std::vector<std::string> skills ) ;
-			void delete_skills( std::vector<std::string> skills ) ;
-
 		// Company
-			Company get_company() ;
-			void setCompany() ;
+			Company get_company() const ;
+			void set_company( Company const& company) ;
+
+		// Skills
+			void print_listSkills() const ;
+			void add_skill( std::string const& skill ) ;
+			void delete_skill( std::string const& skill ) ;
 
 
 	private:
-		std::string _name ;
-		std::vector<std::string> _skills ;
+		std::string _title ;
 		Company _company ;
+		std::vector<std::string> _listSkills ;		
 } ;
 
 
