71dfa16e50b1216d5d23ccbed966e2014c7b0019
GNANKENE <cheikh-ahmed-t.GNANKENE@etu.univ-amu.fr>
2020-04-06 04:11:51 +0200

    Ajout de fonction sup.h

---
+++ lib/fonctionSup.h
@@ -0,0 +1,25 @@
+//Pour les fonctions Supplementaires
+//last modif: GNANKENE Cheick   06/04/2020
+//=========================================
+
+
+#ifndef __FONCTIONSUP__
+#define __FONCTIONSUP__
+
+#include <iostream>
+#include <vector>
+
+
+//////////////FOCTION DE DIALOGUE DU MENU///////////////
+void menu_entreprise();
+void menu_employe();
+void menu_chercheur_emploi();
+void menu_gen();
+void rechercher_profil_v_entreprise();
+void modifier_profil_employe();
+void modifier_profil_chercheur();
+void recherche_ancien_col();
+void rechercher_entreprise();
+////////////////////////////////////////////////////////
+
+#endif
