541d2a06c5e62173b68a87668db9b924b0957c5e
Hanry Nzale <hanry.dev@gmail.com>
2020-04-13 14:07:08 +0200

    creation d'un fichier pour les constantes

---
+++ lib/constante.h
@@ -0,0 +1,14 @@
+#ifndef CONSTANTE_H 
+#define CONSTANTE_H
+
+//CODES DE RETOUR
+
+//Erreur à l'ouverture du fichier
+const int OPEN_FILE_ERROR = -1;
+
+//Tout s'est bien passé
+const int SUCCESS = 0;
+
+//Une fonction appelée dans la fonction a retourné un code erreur
+const int SUB_FUNCTION_ERROR = -2;
+#endif
