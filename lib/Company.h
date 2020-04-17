d92a54a2e20dfc8507212a01d84db1c44fade84a
GNANKENE <cheikh-ahmed-t.GNANKENE@etu.univ-amu.fr>
2020-04-17 18:53:41 +0200

    Ajout de company.h v2

--- lib/Company.h
+++ lib/Company.h
@@ -21,19 +21,16 @@ class Company
 			// Creation
 				Company() ;																					// Create the profil of the company
 				Company( std::string const& name, int const& postalCode, std::string const& email ) ;						//
-				int create_company();
-				int delete_company();
-			// Deletion
-				void delete_profile() ;																			// Delete the profile of the company
-				~Company() ;																					// (  IL FAUDRA NOTAMMENT DETRUIRE '_listSkills' PROPREMENT DE SORTE QU'IL N'Y AIT PAS DE FUITE DE MEMOIRE  )
-
+				void create_company();
+				void delete_company();
+			
 			// Job vacancy																				
-				int create_profile_jobVacancy() ;			// Create a job vacancy and add it to "_listJobsVacancy"			
-				int delete_profile_jobVacancy() ;											// Delete of "_listJobsVacancy"  the job vacancy passed in argument
+				void create_profile_jobVacancy() ;			// Create a job vacancy and add it to "_listJobsVacancy"			
+				void delete_profile_jobVacancy() ;											// Delete of "_listJobsVacancy"  the job vacancy passed in argument
 
 			// Search																							// The 2 next fonctions print surname, firstname and email address
-				int search_by_skill( std::vector<std::string> const& skills ) const ;									// Search the job seekers corresponding to the skills			
-				int search_by_skill_postalCode( std::vector<std::string> const& skills, int const& postalCode ) const ;			// Search the job seekers corresponding to the skills and the postal code		
+				void search_by_skill( std::vector<std::string> const& skills ) const ;									// Search the job seekers corresponding to the skills			
+				void search_by_skill_postalCode( std::vector<std::string> const& skills, int const& postalCode ) const ;			// Search the job seekers corresponding to the skills and the postal code		
 		
 																										
 		// Accessors
