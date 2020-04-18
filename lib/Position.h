73142c04f4cf019bb59f03fa1622f270782b01ab
MiharyR <mihary.rnvsn@gmail.com>
2020-04-18 11:20:31 +0200

    correction position.h

--- lib/Position.h
+++ lib/Position.h
@@ -9,7 +9,6 @@ LAST MODIFICATION:            ||
 #ifndef __POSITION__
 #define __POSITION__
 
-#include "Company.h"
 #include <iostream>
 #include <vector>
 
@@ -18,21 +17,17 @@ class Position
 {
 	public:
 		// Creation
-			Position( std::string const& title, Company const& company ) ;
-			Position( std::string const& title, Company const& company, std::vector<std::string> const& skills ) ;
+			Position( std::string const& title ) ;		
+			Position( std::string const& title, std::vector<std::string> const& skills ) ;
 
 		// Deletion
 			void delete_position() ;
-			~Position() ;																				// (  IL FAUDRA NOTAMMENT DETRUIRE '_listSkills' PROPREMENT DE SORTE QU'IL N'Y AIT PAS DE FUITE DE MEMOIRE  )
+		//	~Position() ;																				// 
 
 		// Name
 			std::string get_title() const ;
 			void set_title( std::string const& title ) ;
 		
-		// Company
-			Company get_company() const ;
-			void set_company( Company const& company) ;
-
 		// Skills
 			void print_listSkills() const ;
 			void add_skill( std::string const& skill ) ;
@@ -41,7 +36,6 @@ class Position
 
 	private:
 		std::string _title ;
-		Company _company ;
 		std::vector<std::string> _listSkills ;		
 } ;
 
